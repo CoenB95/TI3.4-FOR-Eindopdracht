eb2ac63951c8390acece4452f2346b942e371474
CoenB95 <boelhouwers.coen@gmail.com>
2019-04-23 15:45:46 +0200

    Initial commit.

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:TI3/TI3.dart' as TI3;
+
+main(List<String> arguments) {
+  print('Hello world: ${TI3.calculate()}!');
+}
---
+++ test/TI3_test.dart
@@ -0,0 +1,8 @@
+import 'package:TI3/TI3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
